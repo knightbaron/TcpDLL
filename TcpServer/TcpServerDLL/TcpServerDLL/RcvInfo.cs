9750f1b84a6df2d007e82c9a1aabc28496d1e4ce
knightbaron <62199759+knightbaron@users.noreply.github.com>
2024-10-14 17:36:42 +0900

    Add files via upload

---
+++ TcpServer/TcpServerDLL/TcpServerDLL/RcvInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServerDLL
+{
+    /// <summary>
+    /// TCPサーバー受信情報
+    /// </summary>
+    public class TcpServerRcvInfo
+    {
+        /// <summary>
+        /// 受信時刻
+        /// </summary>
+        public DateTime Date;
+
+        /// <summary>
+        /// 受信元情報
+        /// </summary>
+        public TcpClient Client;
+
+        /// <summary>
+        /// 受信内容
+        /// </summary>
+        public string RcvBuffer = null;
+
+        /// <summary>
+        /// 受信発生
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="rcvBuffer"></param>
+        public TcpServerRcvInfo(TcpClient client, string rcvBuffer)
+        {
+            Date = DateTime.Now;
+            Client = client;
+            RcvBuffer = rcvBuffer;
+        }
+
+
+    }
+}
